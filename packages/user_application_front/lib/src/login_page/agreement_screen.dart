7a8cfda574a9efa5b16bcc2db93e6e05566fd11e
Baekho Kim <0110kim20@kaist.ac.kr>
2021-10-07 23:55:19 +0900

    [feat] 이용약관 페이지 틀 제작
    이용약관 페이지 틀 제작

---
+++ packages/user_application_front/lib/src/login_page/agreement_screen.dart
@@ -0,0 +1,84 @@
+import 'package:flutter/material.dart';
+
+class AgreementScreen extends StatefulWidget {
+  @override
+  _AgreementScreen createState() => _AgreementScreen();
+}
+
+class _AgreementScreen extends State<AgreementScreen> {
+  @override
+  Widget build(BuildContext context) {
+    final valHeight = MediaQuery.of(context).size.height; //화면 높이
+    final valWidth = MediaQuery.of(context).size.width; //화면 너비
+    var fontSize2 = 20;
+    return Scaffold(
+        appBar: AppBar(
+          title: Text("개인정보 처리방침 및 이용약관"),
+        ),
+        body: Center(
+          child: ListView(
+            children: [
+              Column(
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: [
+                  SizedBox(
+                    height: valHeight * 0.1,
+                  ),
+                  Container(
+                    child: Text(
+                      "App 이용약관",
+                      style: TextStyle(
+                        fontSize: 20,
+                      ),
+                    ),
+                    margin: EdgeInsets.fromLTRB(0, 0, 0, 5),
+                  ),
+                  Container(
+                    //이용약관 들어갈 곳
+                    child: Text("여기에 이용약관이 적혀야 합니다."),
+                    color: Colors.grey,
+                    height: valHeight * 0.3,
+                    width: valWidth * 0.8,
+                  ),
+                  SizedBox(
+                    height: valHeight * 0.1,
+                  ),
+                  Container(
+                    child: Text(
+                      "개인정보 처리방침",
+                      style: TextStyle(
+                        fontSize: 20,
+                      ),
+                    ),
+                    margin: EdgeInsets.fromLTRB(0, 0, 0, 5),
+                  ),
+                  Container(
+                      //개인정보 처리방침 들어갈 곳
+                      child: Text("여기에 개인정보 처리방침이 적혀야 합니다."),
+                      color: Colors.grey,
+                      height: valHeight * 0.3,
+                      width: valWidth * 0.8),
+                  SizedBox(
+                    height: valHeight * 0.1,
+                  ),
+                  Container(
+                      child: Text(
+                        "개인(신용)정보 수집 및 이용동의",
+                        style: TextStyle(
+                          fontSize: 20,
+                        ),
+                      ),
+                      margin: EdgeInsets.fromLTRB(0, 0, 0, 5)),
+                  Container(
+                      //개인정보 수집 및 이용동의 들어갈 곳
+                      child: Text("여기에 개인정보 수집 및 이용동의가 적혀야 합니다."),
+                      color: Colors.grey,
+                      height: valHeight * 0.3,
+                      width: valWidth * 0.8),
+                ],
+              ),
+            ],
+          ),
+        ));
+  }
+}
---
+++ packages/user_application_front/lib/src/login_page/basic_info_screen.dart
@@ -0,0 +1,38 @@
+/*import 'package:flutter/material.dart';
+
+class AgreementScreen extends StatefulWidget {
+  @override
+  _AgreementScreen createState() => _AgreementScreen();
+}
+
+class _AgreementScreen extends State<AgreementScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: Center(
+      child: Column(
+        children: [],
+      ),
+    ));
+  }
+}
+*/ // 화면 새로 만들때 사용. 개발 편의를 위한 복붙용임.
+
+import 'package:flutter/material.dart';
+
+class BasicInfoScreen extends StatefulWidget {
+  @override
+  _BasicInfoScreen createState() => _BasicInfoScreen();
+}
+
+class _BasicInfoScreen extends State<BasicInfoScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: Center(
+      child: Column(
+        children: [],
+      ),
+    ));
+  }
+}
