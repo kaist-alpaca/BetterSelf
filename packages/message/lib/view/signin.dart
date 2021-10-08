465610082a9cbd7fd59aba5bc24efc174e92032c
서경덕 <rudejr0534@kaist.ac.kr>
2021-10-09 08:24:20 +0900

    [feat]사용자계정의 firebase내 등록 및 검색 구현
    서버내 등록된 사용자 계정을 querystream으로 받아오는 형식
    chating기능은 아직 개발중에 있음

---
+++ packages/message/lib/view/signin.dart
@@ -0,0 +1,64 @@
+import 'package:flutter/material.dart';
+import 'package:message/widgets/widget.dart';
+
+class signin extends StatefulWidget {
+  const signin({Key? key}) : super(key: key);
+  @override
+  _signinState createState() => _signinState();
+}
+
+class _signinState extends State<signin> {
+
+  bool isLoading = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('A Title'),
+      ),
+      body: Container(
+        alignment: Alignment.bottomCenter,
+        padding : EdgeInsets.symmetric(horizontal: 24),
+        child : Column(
+          children: [
+            TextField(
+              style: TextStyle(color: Colors.white),
+              decoration: textFieldInput("Email"),
+            ),
+            TextField(
+              style: TextStyle(color: Colors.white),
+              decoration: textFieldInput("Password"),
+            ),
+            SizedBox(height: 8,),
+            SizedBox(height: 8,),
+            Container(
+              alignment: Alignment.center,
+              width: MediaQuery.of(context).size.width,
+              padding: EdgeInsets.symmetric(vertical: 20),
+              decoration: const BoxDecoration(
+                  gradient: LinearGradient(
+                      colors: [
+                        Color(0xff007EF4),
+                        Color(0xff2A75BC)
+                      ]
+                  )
+              ),
+              child: Text("Sign Up", style: TextStyle(color: Colors.white)),
+            ),
+            SizedBox(height: 16,),
+            Container(
+              alignment: Alignment.center,
+              width: MediaQuery.of(context).size.width,
+              padding: EdgeInsets.symmetric(vertical: 20),
+              decoration: const BoxDecoration(
+                  color: Colors.white,
+              ),
+              child: Text("Sign with Google", style: TextStyle(color: Colors.black)),
+            ),
+          ]
+          )
+        )
+      );
+  }
+}
