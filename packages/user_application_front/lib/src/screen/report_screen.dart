40f2c6a40580ae326183d69d2d6d6bc818c89146
Baekho Kim <0110kim20@kaist.ac.kr>
2021-10-09 02:51:47 +0900

    [feat] 리포트 페이지 1차적 틀 구현
    리포트 페이지 1차적 틀 구현. (UI 미완이라 기본틀만)

--- packages/user_application_front/lib/src/screen/report_screen.dart
+++ packages/user_application_front/lib/src/screen/report_screen.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:user_application_front/src/pages/report_page/Inbody_page/inbody_screen.dart';
+import 'package:user_application_front/src/pages/report_page/inbody_page/inbody_screen.dart';
+import 'package:user_application_front/src/pages/report_page/food_page/food_screen.dart';
+import 'package:user_application_front/src/pages/report_page/exercise_page/exercise_screen.dart';
 
 class ReportScreen extends StatefulWidget {
   @override
@@ -14,27 +16,99 @@ class _ReportScreen extends State<ReportScreen> {
     final valWidth = MediaQuery.of(context).size.width; //화면 너비
     double defaultSize = valWidth * 0.0025;
     return Scaffold(
-      appBar: AppBar(
-        title: Text("Report"),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.start,
-          children: <Widget>[
-            GestureDetector(
-              onTap: () {
-                Navigator.push(context,
-                    MaterialPageRoute(builder: (context) => InbodyScreen()));
-              },
-              child: Container(
-                color: Colors.grey,
-                width: valWidth,
-                height: valHeight * 0.5,
-              ),
-            )
-          ],
+        appBar: AppBar(
+          title: Text("Report"),
         ),
-      ),
-    );
+        body: ListView(children: [
+          Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                Container(
+                  child: Text(
+                    '코칭',
+                    style: TextStyle(fontSize: defaultSize * 12),
+                  ),
+                  height: valHeight * 0.05,
+                  margin: EdgeInsets.only(
+                      left: valWidth * 0.14, top: valHeight * 0.03),
+                ),
+                Container(
+                  child: Text('여기에는 코칭 내용이 들어갈 예정입니다.'),
+                  decoration: BoxDecoration(
+                      border: Border.all(color: Colors.black, width: 3)),
+                  width: valWidth * 0.84,
+                  margin: EdgeInsets.only(left: valWidth * 0.08),
+                  height: valHeight * 0.23,
+                ),
+                Container(
+                  child: Text(
+                    '데이터',
+                    style: TextStyle(fontSize: defaultSize * 12),
+                  ),
+                  height: valHeight * 0.05,
+                  margin: EdgeInsets.only(
+                      left: valWidth * 0.14, top: valHeight * 0.03),
+                ),
+                GestureDetector(
+                  //여기를 누르면 인바디 및 기타등등으로 넘어감. (pages-> reportpage 참조.)
+                  onTap: () {
+                    Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                            builder: (context) => InbodyScreen()));
+                  },
+                  child: Container(
+                    color: Colors.grey,
+                    height: 0.4 * valHeight,
+                    width: valWidth * 0.84,
+                    margin: EdgeInsets.only(left: valWidth * 0.08),
+                    child: Text('여긴 인바디데이터'),
+                  ),
+                ),
+                SizedBox(height: valHeight * 0.02),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    GestureDetector(
+                      onTap: () {
+                        Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                                builder: (context) => ExerciseScreen()));
+                      },
+                      child: Container(
+                        width: valWidth * 0.4,
+                        height: valHeight * 0.2,
+                        color: Colors.grey,
+                        child: Text('여기에 운동 그래프가 들어가야 합니다.'),
+                      ),
+                    ),
+                    SizedBox(width: valWidth * 0.04),
+                    GestureDetector(
+                      onTap: () {
+                        Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                                builder: (context) => FoodScreen()));
+                      },
+                      child: Container(
+                        width: valWidth * 0.4,
+                        height: valHeight * 0.2,
+                        color: Colors.grey,
+                        child: Text('여기에 식단 기록이 들어가야 합니다.'),
+                      ),
+                    )
+                  ],
+                ),
+                SizedBox(
+                  height: valHeight * 0.05,
+                )
+              ],
+            ),
+          ),
+        ]));
   }
 }
