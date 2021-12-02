c28d6f02b3ae2d17de4f3e153feca800b3579f31
2 <alvinlog@kaist.ac.kr>
2021-12-02 23:36:25 +0900

    [feat] fcm 메시지 수신 기능 구현

--- releases/betterme/lib/betterme/home/functions/ConstructTabBar.dart
+++ releases/betterme/lib/betterme/home/functions/ConstructTabBar.dart
@@ -1,3 +1,4 @@
+import 'package:betterme/betterme/communication/chatroom/ChatroomScreen.dart';
 import 'package:betterme/betterme/home/HomeScreen.dart';
 import 'package:betterme/functions/Controllers/server_connection.dart';
 import 'package:flutter/cupertino.dart';
@@ -7,6 +8,7 @@ import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:betterme/betterme/communication/CommunicationScreen.dart';
 import 'package:betterme/betterme/report/ReportScreen.dart';
 import 'package:betterme/betterme/setting/SettingScreen.dart';
+import 'package:get/get.dart';
 import 'package:keyboard_actions/external/platform_check/platform_check.dart';
 
 class ConstructTabBar extends StatefulWidget {
@@ -18,51 +20,107 @@ class ConstructTabBar extends StatefulWidget {
 
 class _ConstructTabBar extends State<ConstructTabBar> {
   late PageController pageController;
-  int getPageIndex = 0;
-  int checker = 0;
 
   @override
   void initState() {
     super.initState();
     ServerConnection.write_log('ConstructTabBar', '', '');
     pageController = PageController();
+    FirebaseMessaging.onMessageOpenedApp.listen((RemoteMessage message) {
+      print(message.notification!.body);
+      print(message.data);
+      print(message.data['from_who']);
+      print(message.data.toString());
+      print(message.notification!.title);
+      print('A new onMessageOpenedApp event was published!');
+      // setState(() {
+      //   this.getPageIndex = 1;
+      // });
+      // pageController.animateToPage(1,
+      //     duration: Duration(milliseconds: 100), curve: Curves.bounceInOut);
+      // ConstructTabBar(getPageIndex: 1);
+      // ConstructTabBar(getPageIndex: 1);
+      Navigator.push(
+          context,
+          MaterialPageRoute(
+              builder: (context) => ChatroomScreen(
+                  message.data['tousernamechatwith_uid'],
+                  message.data['namechatwith'],
+                  message.data['imgUrl'],
+                  message.data['send_uid'])));
+      // Navigator.push(
+      //     context,
+      //     MaterialPageRoute(
+      //         builder: (context) => ChatroomScreen(
+      //             message.data['namechatwith'],
+      //             message.data['tousernamechatwith_uid'],
+      //             message.data['imgUrl'],
+      //             message.data['send_uid'])));
+    });
   }
 
-  onTapChangePage(int pageIndex) {
-    setState(() {
-      this.getPageIndex = pageIndex;
-    });
-    pageController.animateToPage(pageIndex,
-        duration: Duration(milliseconds: 100), curve: Curves.bounceInOut);
-  }
+  // onTapChangePage(int pageIndex) {
+  //   setState(() {
+  //     this.getPageIndex = pageIndex;
+  //   });
+  //   pageController.animateToPage(pageIndex,
+  //       duration: Duration(milliseconds: 100), curve: Curves.bounceInOut);
+  // }
+
+  List<BottomNavigationBarItem> items = [
+    BottomNavigationBarItem(icon: SvgPicture.asset('images/report_icon.svg')),
+    BottomNavigationBarItem(
+        icon: SvgPicture.asset('images/communication_icon.svg')),
+    BottomNavigationBarItem(icon: SvgPicture.asset('images/setting_icon1.svg')),
+  ];
 
   buildHomeScreen() {
-    return Scaffold(
-      body: PageView(
-        children: <Widget>[
-          ReportScreen(),
-          CommunicationScreen(),
-          SettingScreen()
-        ],
-        controller: pageController, // controller를 지정해주면 각 페이지별 인덱스로 컨트롤 가능
-        physics: NeverScrollableScrollPhysics(),
-      ),
-      bottomNavigationBar: CupertinoTabBar(
-        currentIndex: getPageIndex,
-        onTap: onTapChangePage,
-        activeColor: Colors.white,
-        inactiveColor: Color(0xff6E6572),
-        backgroundColor: Color(0xff0B202A),
-        items: [
-          BottomNavigationBarItem(
-              icon: SvgPicture.asset('images/report_icon.svg')),
-          BottomNavigationBarItem(
-              icon: SvgPicture.asset('images/communication_icon.svg')),
-          BottomNavigationBarItem(
-              icon: SvgPicture.asset('images/setting_icon1.svg')),
-        ],
-      ),
-    );
+    // return Scaffold(
+    //   body: PageView(
+    //     children: <Widget>[
+    //       ReportScreen(),
+    //       CommunicationScreen(),
+    //       SettingScreen()
+    //     ],
+    //     controller: pageController, // controller를 지정해주면 각 페이지별 인덱스로 컨트롤 가능
+    //     physics: NeverScrollableScrollPhysics(),
+    //   ),
+    // bottomNavigationBar: CupertinoTabBar(
+    //   currentIndex: getPageIndex,
+    // onTap: onTapChangePage,
+    // activeColor: Colors.white,
+    // inactiveColor: Color(0xff6E6572),
+    // backgroundColor: Color(0xff0B202A),
+    // items: [
+    //   BottomNavigationBarItem(
+    //       icon: SvgPicture.asset('images/report_icon.svg')),
+    //   BottomNavigationBarItem(
+    //       icon: SvgPicture.asset('images/communication_icon.svg')),
+    //   BottomNavigationBarItem(
+    //       icon: SvgPicture.asset('images/setting_icon1.svg')),
+    // ],
+    //   ),
+    // );
+    return CupertinoTabScaffold(
+        tabBar: CupertinoTabBar(
+            items: items,
+            currentIndex: 0,
+            //onTap: onTapChangePage,
+            activeColor: Colors.white,
+            inactiveColor: Color(0xff6E6572),
+            backgroundColor: Color(0xff0B202A)),
+        tabBuilder: (context, index) {
+          switch (index) {
+            case 0:
+              return ReportScreen();
+            case 1:
+              return CommunicationScreen();
+            case 2:
+              return SettingScreen();
+            default:
+              return ReportScreen();
+          }
+        });
   }
 
   @override
