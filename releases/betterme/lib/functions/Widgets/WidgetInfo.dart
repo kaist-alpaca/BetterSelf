aad19d2b56babf64d0014b167beb62bf0aab06bf
정영민 <james09756@kaist.ac.kr>
2021-12-13 05:50:27 +0900

    [restructure] 프론트엔드에 관한 정보를 따로 빼놓은 widgetinfo 작성

--- releases/betterme/lib/functions/Widgets/WidgetInfo.dart
+++ releases/betterme/lib/functions/Widgets/WidgetInfo.dart
@@ -26,38 +26,97 @@ class ComponentInfo {
   );
 }
 
-//홈 화면의 주간코칭 UI components의
+//홈 화면의 그래프1, 2를 구성하는 UI component의
 //크기 및 색상, 텍스트 정보 등을 정해주는 클래스
-class HomeInfo {
+class HomeGraphInfo {
+  double valWidth;
+  double valPixel;
+  late final double widthRatio; //피그마의 수치를 실제 픽셀값으로 바꿔주는 비율
+
+  //스케일바 관련
+  late final double indent_scalebar;
+
+  //태그 관련
+  late final double indent_tag; //태그 컨테이너의 들여쓰기 정도
+  late final double tag_height; //태그 높이
+  late final double tag_width; //태그 너비
+  late final Radius tag_boundaryRad; //태그 모서리 곡률
+  late final double tag_boundaryWid; //태그 모서리 두께
+  late final List<Color> tag_boundaryColor; //태그 모서리 색깔
+  late final Color tag_colorDefault; //태그 배경색
+  late final Color tag_colorClicked; //클릭됐을 때의 태그 배경색
+  late final TextStyle text_tag; // 태그 제목 정보
+  late final TextStyle text_subtag; // 태그 부제목 정보
+  late final double blank_tags; //각 태그 사이의 간격
+  late final double blank_spaceTags; //위 태그와 아래 태그 사이의 간격
+
+  late final double blank_tagcontainer; //태그와 그래프1 사이의 간격
+
+  HomeGraphInfo(this.valWidth, {this.valPixel = 364}) {
+    this.widthRatio = valWidth / valPixel;
+
+    indent_tag = 40 * widthRatio;
+    tag_height = 22;
+    tag_width = 65 * widthRatio;
+    tag_boundaryRad = Radius.circular(5.0);
+    tag_boundaryWid = 1;
+    tag_boundaryColor = [
+      Color(0xff858E93), //전체
+      Color(0xffFFFDFD), //체중
+      Color(0xffA0B1DF), //수면
+      Color(0xffF2D8A7), //스트레스
+      Color(0xffD2ABBA), //섭취
+      Color(0xff8DBFBC), //소모
+    ];
+    tag_colorDefault = Colors.transparent;
+    tag_colorClicked = Color(0xff3D434E);
+    text_tag = TextStyle(
+      fontFamily: "NotoSans",
+      fontSize: 12,
+      color: Color(0xffFFFDFD),
+    );
+    text_subtag = TextStyle(
+      fontFamily: "NotoSans",
+      fontSize: 9,
+      color: Color(0xffFFFDFD),
+    );
+    blank_tags = 8 * widthRatio;
+    blank_spaceTags = 10 * widthRatio;
+
+    blank_tagcontainer = 24;
+  }
+}
+
+//홈 화면의 코칭 UI components의
+//크기 및 색상, 텍스트 정보 등을 정해주는 클래스
+class HomeCoachingInfo {
   double valWidth;
   double valPixel;
 
-  late double widthRatio; //피그마의 수치를 실제 픽셀값으로 바꿔주는 비율
+  late final double widthRatio; //피그마의 수치를 실제 픽셀값으로 바꿔주는 비율
 
-  late double indent_coaching;
-  late double box_coaching;
-  late TextStyle text_coaching;
+  late final double indent_coaching; //"oo데이터 코칭+날짜" 컨테이너의 들여쓰기 크기
+  late final double box_coaching; //"oo데이터 코칭" 텍스트 컨테이너 사이즈
+  late final TextStyle text_coaching; //"oo데이터 코칭" 텍스트 정보
 
-  late double box_selectedDate;
-  late TextStyle text_selectedDate;
+  late final double box_selectedDate; //현재 선택한 날짜의 텍스트 컨테이너 사이즈
+  late final TextStyle text_selectedDate; //현재 선택한 날짜의 텍스트 정보
 
-  late double blank_coachingInfo;
-  late double indent_coachingDivider;
-  late Divider coachingDivider;
+  late final double blank_coachingInfo; //코칭 디바이더와의 높이 간격
+  late final Divider coachingDivider; //코칭 디바이더 정보
 
-  HomeInfo(this.valWidth, {this.valPixel = 364}) {
+  //실제 context의 크기에 맞춰 값 생성
+  HomeCoachingInfo(this.valWidth, {this.valPixel = 364}) {
     this.widthRatio = valWidth / valPixel;
 
-    indent_coaching = 40 * widthRatio; //"oo데이터 코칭+날짜" 컨테이너의 들여쓰기 크기
-    box_coaching = 100 * widthRatio; //"oo데이터 코칭" 텍스트 컨테이너 사이즈
-    text_coaching = ComponentInfo.smallTitle; //"oo데이터 코칭" 텍스트 정보
+    indent_coaching = 40 * widthRatio;
+    box_coaching = 100 * widthRatio;
+    text_coaching = ComponentInfo.smallTitle;
 
-    box_selectedDate = 68 * widthRatio; //현재 선택한 날짜의 텍스트 컨테이너 사이즈
-    text_selectedDate = ComponentInfo.content; //현재 선택한 날짜의 텍스트 정보
+    box_selectedDate = 68 * widthRatio;
+    text_selectedDate = ComponentInfo.content;
+    blank_coachingInfo = 10.0;
 
-    blank_coachingInfo = 10.0; //코칭 디바이더와의 높이 간격
-
-    //코칭 디바이더 정보
     coachingDivider = Divider(
       indent: 24 * widthRatio,
       endIndent: 24 * widthRatio,
