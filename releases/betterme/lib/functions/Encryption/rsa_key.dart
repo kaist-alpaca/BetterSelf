6283fe7a3309b5caff2df8a900488dcbc61b354f
2 <alvinlog@kaist.ac.kr>
2021-12-13 05:44:33 +0900

    [feat] 암호화를 위한 코드

---
+++ releases/betterme/lib/functions/Encryption/rsa_key.dart
@@ -0,0 +1,4 @@
+class rsa_key {
+  static final public_key =
+      "-----BEGIN PUBLIC KEY-----\nMIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEA4gIG7Vv2RCySB/d9v09l\nx9hj7ojM06iBPTRiT1u/JbsRCy0ypt0Jfz1mOYHANvNaQcnesE7ScjxQbOWtcsVs\nwsfKzdaoZOf8ExO/Lls2u68Q8E+YnU1RtxWxJV8j3dDYKgfWeMIvzxZRCS6fexfC\naHnD6W/B37UPbEpRD+Ho15jWpkDxKxjZuldavdEZ6/hHKg3xvEfeHuAvQTWGlCf6\ntisROaHbQRMRgfn+CI86xzrP12TOfLgMed40prYoAdU3QpBDZMAoyNOve4VPpoOW\nukqO5u6MEQAy3/fXIWy+d8mavvaAWm4bYh5rxj4uyHKwM+d3pMwY4kF10cf5h6Oy\nrQIDAQAB\n-----END PUBLIC KEY-----";
+}
